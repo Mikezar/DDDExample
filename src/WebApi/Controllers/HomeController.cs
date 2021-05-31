31cd6c702ec2a54e2d5cc9f00b77a2f9fa374f76
Mikhail Serdyuk <mike@hopshop.ru>
2021-05-31 16:08:24 +0300

    Init

---
+++ src/WebApi/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi.Controllers
+{
+    [ApiExplorerSettings(IgnoreApi = true)]
+    [ApiController]
+    [Route("/")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Redirect("/swagger");
+        }
+    }
+}
